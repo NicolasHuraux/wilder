00319ad42a98b846ef8e35f3fcc20a9448baf548
NicolasHuraux <n.huraux@hotmail.com>
2019-06-05 16:42:40 +0200

    classroom

---
+++ Classroom.java
@@ -0,0 +1,14 @@
+class Classroom {
+
+    public static void main(String[] args) {
+        Wilder jeanClaude = new Wilder("Jean-Claude", true); 
+        Wilder henri = new Wilder("Henri", false);
+     
+        jeanClaude.whoAmI();
+        henri.whoAmI();     
+
+        
+
+    }
+}
+
